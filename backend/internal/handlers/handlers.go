b3ef99804c80dc42b35a92c496c6947bf7fd591c
Luka <lukapuhlyak8@gmail.com>
2025-12-03 19:46:02 +0300

    add swagger and connect to redis

--- backend/internal/handlers/handlers.go
+++ backend/internal/handlers/handlers.go
@@ -2,15 +2,26 @@ package handlers
 
 import (
 	"backend/internal/types"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+// takeToken godoc
+// @Summary Takes Telegram token
+// @Description Validate tg token and return login result
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param input body types.Token true "Token"
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /api/token [post]
 func takeToken(c *gin.Context) {
 	var req types.Token
 	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
-		c.JSON(http.StatusTeapot, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
@@ -20,5 +31,17 @@ func takeToken(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
+	})
+}
+
+// adminLogin godoc
+// @Summary Admin login
+// @Tags admin
+// @Produce json
+// @Failure 418 {object} map[string]interface{}
+// @Router /admin/api/login [post]
+func adminLogin(c *gin.Context) {
+	c.JSON(http.StatusTeapot, gin.H{
+		"error": errors.New("lol I don't make it"),
 	})
 }
--- backend/internal/handlers/server.go
+++ backend/internal/handlers/server.go
@@ -1,11 +1,62 @@
 package handlers
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gomodule/redigo/redis"
+	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+var REDISUSER string
+var REDISPASS string
+var redisConn redis.Conn
 
 func Start_server() {
 	r := gin.Default()
+	LoadEnv()
 	r.POST("/api/token", takeToken)
+	r.POST("/admin/api/login", adminLogin)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	connectToRedis()
+	defer redisConn.Close()
 	r.Run("0.0.0.0:8080")
+}
+
+func connectToRedis() {
+	var err error
+	redisConn, err = redis.Dial("tcp", "localhost:6379",
+		redis.DialUsername(REDISUSER),
+		redis.DialPassword(REDISPASS),
+	)
+	if err != nil {
+		panic(fmt.Sprintf("err with connect to redis; err %s", err))
+	}
+
+}
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	REDISUSER = getFromEnv("REDISUSER", "admin")
+
+	REDISPASS = getFromEnv("REDISPASS", "some_pass")
+}
+
+func getFromEnv(variadle string, defaultVariable string) string {
+	variab := os.Getenv(variadle)
+	if variab == "" {
+		variab = defaultVariable
+	}
+	fmt.Printf("%s:%s", variadle, variab)
+	return variab
 }
